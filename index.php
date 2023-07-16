398c9d69fcc4bc55e5db63b7a285ab556a05a792
ronaldotrisaputra <ronaldo291201@gmail.com>
2023-07-17 03:13:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam officia voluptatum perferendis, sunt eos error et perspiciatis atque doloremque. Mollitia molestias pariatur veniam blanditiis quaerat ipsa similique quae error non.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
